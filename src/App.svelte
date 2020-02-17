692ce2e984a071b826de595ece9c85c9fab7e470
Vince Gabriel Lagarto <vince@dentistfind.com>
2020-02-17 10:42:48 +0800

    first code commit

---
+++ src/App.svelte
@@ -0,0 +1,69 @@
+<script>
+  import Recipe from "./Recipe.svelte";
+  import Spinner from "./Spinner.svelte";
+  let fetching = false;
+</script>
+
+<style>
+  main {
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+    background-color: #071a52;
+    color: #a7ff83;
+    height: 100%;
+  }
+
+  .recipes-container {
+    display: flex;
+    margin-top: 50px;
+  }
+
+  .search-container {
+    text-align: center;
+  }
+
+  .search-input {
+    width: 30%;
+    color: #071a52;
+    padding: 10px;
+    font-size: 20px;
+  }
+
+  .btn-search {
+    padding: 10px;
+    font-size: 20px;
+  }
+
+  .spinner-container {
+    margin-top: 100px;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
+<main>
+  <div class="search-container">
+    <h1>Recipebook App</h1>
+    <input type="text" placeholder="Search for Recipe" class="search-input" />
+    <input type="submit" value={'🔍'} class="btn-search" />
+  </div>
+  {#if fetching}
+    <center>
+      <div class="spinner-container">
+        <h4>Searching..</h4>
+        <br />
+        <Spinner />
+      </div>
+    </center>
+  {:else}
+    <div class="recipes-container">
+      <Recipe />
+      <Recipe />
+    </div>
+  {/if}
+</main>
---
+++ src/Recipe.svelte
@@ -0,0 +1,27 @@
+<script>
+
+</script>
+
+<style>
+  .container {
+    width: 50%;
+    border: 2px solid #17b978;
+    border-radius: 10px;
+    height: auto;
+    margin: 10px;
+    padding: 10px;
+    background-color: #086972;
+  }
+  h1 {
+    text-align: center;
+  }
+</style>
+
+<div class="container">
+  <h1>Title</h1>
+  <ul>
+    <li>Sample 1</li>
+    <li>Sample 2</li>
+    <li>Sample 3</li>
+  </ul>
+</div>
